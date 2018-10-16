f3a5f68362c79cf31f08162c3be0c43604307794
Bojne <bojne.john@gmail.com>
2018-10-16 13:33:37 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => ( <
+    Layout >
+    <
+    h1 > JAMstack, I 'm coming!</h1> <
+    p > I feel so excited to build a website in my computer. < /p> <
+    p > Moving forward, build more things and meet creative people. < /p> <
+    Link to = "/page-2/" > Go to page 2 < /Link> <
+    /Layout>
+)
+
+export default IndexPage
